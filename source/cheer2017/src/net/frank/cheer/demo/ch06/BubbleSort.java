f068e53e4e3d891dcec4ef463ccbb0927271486e
pennyji <1334611717@qq.com>
2017-04-16 11:06:36 +0800

    update

--- source/cheer2017/src/net/frank/cheer/demo/ch06/BubbleSort.java
+++ source/cheer2017/src/net/frank/cheer/demo/ch06/BubbleSort.java
@@ -1,37 +1,29 @@
 package net.frank.cheer.demo.ch06;
 
 public class BubbleSort {
-
-	public static void main(String[] args) {
-		
-		int [] values = new int[6];
-		values[0] = 8;
-		values[1] = 39;
-		values[2] = 17;
-		values[3] = 78;
-		values[4] = 82;
-		values[5] = 52;
-		
-		
-		for(int i=1;i<values.length;i++){
-			for(int j=0;j<i;j++){
-				if(values[i]<values[j]){
-					int temp = values[i];
-					values[i] = values[j];
-					values[j] = temp;
+	public static void main(String[] args){
+		int [] arr = {6,3,8,2,9,1};
+		System.out.println("≈≈–Ú«∞");
+		printArray(arr);
+		for(int i=0;i< arr.length -1;i++){
+			for(int j=0;j<arr.length-1-i;j++){
+				if(arr[j] > arr[j+1]){
+					arr[j] = arr[j]+ arr[j+1];
+					arr[j+1] = arr[j] - arr[j+1];
+					arr[j] = arr[j] - arr[j+1];
 				}
-				printArray(values);
 			}
+			printArray(arr);
 		}
-		printArray(values);
+		System.out.println("≈≈–Ú∫Û");
+		printArray(arr);
 	}
 	
-	private static void printArray(int[] array){
-		for(int i=0;i<array.length;i++){
-			System.out.print(array[i] + "\t");
+	private static void printArray(int[] arr){
+		for(int num:arr){
+			System.out.print(num + "\t");
 		}
 		System.out.println();
 	}
 	
 }
- 
--- source/cheer2017/src/net/frank/cheer/demo/ch06/SelectSort.java
+++ source/cheer2017/src/net/frank/cheer/demo/ch06/SelectSort.java
@@ -1,42 +1,31 @@
 package net.frank.cheer.demo.ch06;
 
 public class SelectSort {
-
-	public static void main(String[] args) {
-		
-		int [] values = new int[6];
-		values[0] = 8;
-		values[1] = 39;
-		values[2] = 17;
-		values[3] = 78;
-		values[4] = 82;
-		values[5] = 52;
-		
-		for (int i = 0;i<values.length-1;i++ ){
-			int max = -1;
-			int maxIndex = -1;
-			for(int j =0;j<values.length-i;j++){
-				int value = values[j];
-				if(value > max){
-					max = value;
-					maxIndex = j;
+	public static void main(String[] args){
+		int [] arr = {6,3,8,2,9,1};
+		System.out.println("排序前");
+		printArray(arr);
+		int index;
+		for(int i=1;i<arr.length;i++){
+			index=0;
+			for(int j=1;j<=arr.length -i;j++){
+				if(arr[j] > arr[index]){
+					index = j;
 				}
 			}
-			int temp = values[values.length-i-1];
-			values[values.length-i-1] = max;
-			values[maxIndex] = temp;
-			
-			
-			printArray(values);
+			int temp = arr[arr.length-i];
+			arr[arr.length-i] = arr[index];
+			arr[index] = temp;
+			printArray(arr);
 		}
-		printArray(values);
+		System.out.println("排序前");
+		printArray(arr);
 	}
 	
-	private static void printArray(int[] array){
-		for(int i=0;i<array.length;i++){
-			System.out.print(array[i] + "\t");
+	private static void printArray(int[] arr){
+		for(int num:arr){
+			System.out.print(num + "\t");
 		}
 		System.out.println();
 	}
-	
 }
