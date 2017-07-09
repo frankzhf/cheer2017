70521f027f4ccbb46fca931b9064a15c16ff5ea2
zhaofeng <442526654@qq.com>
2017-07-09 10:43:47 +0800

    commmit

--- source/cheer2017/src/net/frank/cheer/demo/ch20/DataBaseUtilTest.java
+++ source/cheer2017/src/net/frank/cheer/demo/ch20/DataBaseUtilTest.java
@@ -12,17 +12,7 @@ public class DataBaseUtilTest {
 			public int callback(Connection conn) throws SQLException {
 				boolean rt = conn.createStatement()
 						.execute("insert into sys_account(id,account_no,account_name,amost) values(3,'6454-82328-3222','wangwu',150000)");
-				if(rt)
-					return 0;
-				else
-					return -1;
-			}
-		});
-		
-		DataBaseUtil.execute(new JdbcOperator() {
-			@Override
-			public int callback(Connection conn) throws SQLException {
-				boolean rt = conn.createStatement()
+				rt = conn.createStatement()
 						.execute("update sys_account set amost=200000 where id=3");
 				if(rt)
 					return 0;
@@ -30,7 +20,6 @@ public class DataBaseUtilTest {
 					return -1;
 			}
 		});
-		
 		
 	}
 
